0ac06755432c965b9df56cc9f39dbe3b3ff5c972
Roby Martinez <martinez.robyp@gmail.com>
2022-04-04 13:20:45 -0600

    adds widget component

--- src/components/Widgets/Widgets.js
+++ src/components/Widgets/Widgets.js
@@ -1,9 +1,28 @@
 import './Widgets.css'
 
+import {TwitterTimelineEmbed, TwitterShareButton, TwitterTweetEmbed} from 'react-twitter-embed'
+import SearchIcon from '@material-ui/icons/Search'
+
 function Widgets() {
   return (
     <div className='widgets'>
-        <h2>Widgets</h2>
+      <div className='widgets__input'>
+        <SearchIcon className='widgets__searchIcon' />
+        <input placeholder='Search Twitter' type="text" />
+      </div>
+      <div className='widgets__container'>
+        <h2>What's happening</h2>
+        <TwitterTweetEmbed tweetId='603384079044759552'/>
+        <TwitterTimelineEmbed
+          sourceType='profile'
+          screenName='Twitter'
+          options={{ height: 400 }}
+        />
+        <TwitterShareButton 
+          url='https://www.twitch.tv/robypalmer'
+          options={{text: 'Click here for maximum entertainment', via: 'robypalmertv'}}  
+        />
+      </div>
     </div>
   )
 }
